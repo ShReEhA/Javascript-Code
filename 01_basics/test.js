ec1821534a6ce4a5297624cbe2428738d45f4e3b
SHREEHARI SAHOO <88489285+ShReEhA@users.noreply.github.com>
2024-01-27 15:57:05 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
-console.log("Shreehari")
+const accountId = 1223
+let accountEmail= "shreehari@gmail.com"
+var accountPassword="Shree"
+accountCity = "Odisha"
+let seminar;
+
+// accountId= 2
+
+/*
+prefer not to use var because of issues in let scope and functional scope
+*/
+
+
+
+accountEmail="Hvvc@gmail.com"
+accountPassword="1234"
+accountCity="Jaipur"
+console.log(accountId);
+console.log([accountId,accountCity,accountEmail,accountPassword,seminar])
