d39a618334c120179da38cbb408506c2e96c8e19
SHREEHARI SAHOO <88489285+ShReEhA@users.noreply.github.com>
2024-01-27 15:56:53 +0000

    datatypes

---
+++ 01_basics/js_02.js
@@ -0,0 +1,26 @@
+/*
+"use strict";// treat all js code as newer version
+*/
+// alert(3+3) // we are using node js not browser 
+console.log(3+3)//code readability that is issue
+console.log("Shreehari")
+
+
+let name = "Shreehari"
+let age= 17
+let isLoggedIn=false 
+
+/*
+variables in JS primitive data type
+number 
+bigInt
+String="" 
+boolean= true/false 
+null => standalore value/type 
+undefined=> 
+symbol => unique  
+*/
+
+// console.log(typeof null)
+// console.log(undefined)
+
