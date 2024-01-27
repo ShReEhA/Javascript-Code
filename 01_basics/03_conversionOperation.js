a709c663ee59184e57f1d30aca7b8b6fd933cbb1
SHREEHARI SAHOO <88489285+ShReEhA@users.noreply.github.com>
2024-01-27 16:58:36 +0000

    conversion Of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+/*let score = "33abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+let scoreNum= Number(score)
+console.log(typeof scoreNum)
+console.log(scoreNum)*/
+/*
+let isLogged = ""
+let isLoggedCheck = Boolean(isLogged)
+console.log(typeof isLoggedCheck)
+console.log(isLoggedCheck) */
+
+//1=> true = 0
+// "" = false
+// "Shreehari" = true
+
+
+let num=23
+let StringNum = String(num)
+console.log(StringNum)
+console.log(typeof StringNum)
+
+
+
+
