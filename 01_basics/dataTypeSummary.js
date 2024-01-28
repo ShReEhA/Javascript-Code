dd44bf5471801836ca3ac96fdbec01c16c028556
SHREEHARI SAHOO <88489285+ShReEhA@users.noreply.github.com>
2024-01-28 03:04:31 +0000

    dataType Summary

---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,22 @@
+// primitive dataType
+//7 categories: String, Number, Boolean, null, undefine, symbol, BigInt
+const score = 100
+const scoreValue = 100.3
+const noscore = null
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId)
+
+//ReferenceType (Non-primitive)
+//Array, Objects, Functions 
+
+
+const name = ["Shaktiman", "Rahul", "Seema"];
+
+let myObj= {
+    "age": 24,
+    "syllabus": "science"
+}
+console.log(typeof noscore)
+
